b85c9ec7e4549b3989e1a3f4300d5758db2bdf1c
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-04-26 00:32:53 +0300

    fixed ui elements moving while text in the middle changes

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 const buttons = document.querySelectorAll(".input-button-wrapper");
 const uiPlayerChoice = document.getElementById("player_choice_img");
 const uiComputerChoice = document.getElementById("computer_choice_img");
+const uiRoundResult = document.getElementById("round-result");
 
 let humanChoice = "";
 let computerChoice = "";
+let roundResult = "";
 
 function setComputerChoice() {
   var randomNumber = Math.floor(Math.random() * buttons.length);
@@ -15,6 +17,27 @@ function playRound() {
   humanChoice = this.getAttribute("id");
   uiPlayerChoice.src = "icons/" + humanChoice + ".png";
   uiComputerChoice.src = "icons/" + computerChoice + ".png";
+  checkWinnerRound();
+}
+
+function checkWinnerRound() {
+  if (humanChoice === computerChoice) {
+    roundResult = "DRAW";
+  }
+  //   human lose
+  else if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")
+  ) {
+    roundResult = "Computer Wins!";
+  }
+  //   human wins
+  else {
+    roundResult = "You Win!";
+  }
+
+  uiRoundResult.textContent = roundResult;
 }
 
 for (const button of buttons) {
